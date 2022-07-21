21741d729a585de45c6f7f56bf9f3f2df8a168d0
nvk <rodolfo@rnvk.org>
2022-07-21 10:57:09 -0400

    Update artifacts.sh

--- coldcard/artifacts.sh
+++ coldcard/artifacts.sh
@@ -4,7 +4,7 @@ find ./firmware -type f -name firmware*.bin >&2
 
 DATE=`date +%Y-%m-%d`
 VERSION=5.0.4
-VERSION_STRING="2022-05-27T1500-v${VERSION}"
+VERSION_STRING="2022-07-20T1508-v${VERSION}"
 URL="https://coldcard.com"
 REPO="https://github.com/Coldcard/firmware"
 CHECKSUM_SOURCE="https://raw.githubusercontent.com/Coldcard/firmware/master/releases/signatures.txt"
