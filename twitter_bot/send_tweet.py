847a11762f19267f39b01dca720ea7600627a6ad
Siim <46551195+siim-m@users.noreply.github.com>
2022-08-18 14:59:48 +1000

    use twitter v2 API for tweeting

--- twitter_bot/send_tweet.py
+++ twitter_bot/send_tweet.py
@@ -14,13 +14,29 @@ auth = tweepy.OAuth1UserHandler(
     os.environ["TWITTER_ACCESS_TOKEN_SECRET"],
 )
 
+# Twitter API v1.1 client - for video upload (v2 doesn't have upload endpoint)
 api = tweepy.API(auth)
 
-upload_result = api.media_upload(filename=sys.argv[2], media_category="tweet_video")
+# Twitter API v2 client - for tweeting (cannot use v1.1 for tweeting with "Essential access" Developer account)
+client = tweepy.Client(
+    consumer_key=os.environ["TWITTER_API_KEY"],
+    consumer_secret=os.environ["TWITTER_API_SECRET"],
+    access_token=os.environ["TWITTER_ACCESS_TOKEN"],
+    access_token_secret=os.environ["TWITTER_ACCESS_TOKEN_SECRET"],
+)
 
-print("Video upload result:", upload_result)
+my_screen_name = client.get_me().data.username
+
+upload_result = api.media_upload(filename=sys.argv[2], media_category="tweet_video")
+print(f"Video upload result: {upload_result}\n")
 
 if upload_result.processing_info["state"] == "succeeded":
-    api.update_status(status=tweet_text, media_ids=[upload_result.media_id])
+    tweet = client.create_tweet(text=tweet_text, media_ids=[upload_result.media_id])
+    print(
+        f"Tweeted with video: https://twitter.com/{my_screen_name}/status/{tweet.data['id']}\n"
+    )
 else:
-    api.update_status(status=tweet_text)
+    tweet = client.create_tweet(text=tweet_text)
+    print(
+        f"Tweeted without video: https://twitter.com/{my_screen_name}/status/{tweet.data['id']}\n"
+    )
