051e8c8eefe8dd18642e3a8c67a930aa6adbd4cc
Cristian Heredia <cristianheredia@protonmail.com>
2022-10-26 22:05:37 -0700

    Updated lnd to 0.15.3

--- lnd/artifacts.sh
+++ lnd/artifacts.sh
@@ -2,12 +2,12 @@
 
 DATE=`date +%Y-%m-%d`
 TWITTER_NAME="LND"
-VERSION="v0.15.0-beta"
+VERSION="v0.15.3-beta"
 URL="https://lightning.network/"
 REPO="https://github.com/lightningnetwork/lnd"
 CHECKSUM_SOURCE="https://github.com/lightningnetwork/lnd/releases/download/${VERSION}/manifest-${VERSION}.txt"
 PROJECT="lnd"
-SHA256=`shasum -a 256 lnd/lnd | cut -f 1 -d ' '`
+SHA256=`shasum -a 256 $GOPATH/bin/lnd | cut -f 1 -d ' '`
 
 # Note GITHUB_ environment variables are populated by Github Actions
 ARTIFACT_BASEURL="https://github.com/${GITHUB_REPOSITORY}/raw"
