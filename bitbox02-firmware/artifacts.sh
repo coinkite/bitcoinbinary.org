245189e0cc833f8d1d9eb9f05e32c8c702347c5b
Cristian Heredia <cristianheredia@protonmail.com>
2022-11-06 22:47:43 -0800

    Updated build instructions for bitbox

--- bitbox02-firmware/artifacts.sh
+++ bitbox02-firmware/artifacts.sh
@@ -3,11 +3,11 @@
 DATE=`date +%Y-%m-%d`
 TWITTER_NAME="BitBox02"
 URL="https://shiftcrypto.ch/bitbox02/"
-VERSION="firmware-btc-only/v9.12.0"
+VERSION_STRING="v9.12.0"
 REPO="https://github.com/digitalbitbox/bitbox02-firmware"
 CHECKSUM_SOURCE="https://github.com/digitalbitbox/bitbox02-firmware/releases/tag/${VERSION}"
 PROJECT="bitbox02-firmware"
-SHA256=`shasum -a 256 firmware/stm32/firmware-signed.bin | cut -f 1 -d ' '`
+SHA256=$(shasum -a 256 bitbox02-firmware/releases/temp/build/bin/firmware-btc.bin | cut -f 1 -d ' ')
 
 # Note GITHUB_ environment variables are populated by Github Actions
 ARTIFACT_BASEURL="https://github.com/${GITHUB_REPOSITORY}/raw"
