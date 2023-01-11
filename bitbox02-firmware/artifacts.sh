05bf1c211d452399e5a7282cf3e7cfc5f49bbc82
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-01-11 09:52:31 -0500

    bitbox02-firmware/artifacts.sh: bump VERSION="firmware-btc-only/v9.13.1"
    Co-authored-by: @caheredia <caheredia@users.noreply.github.com>

--- bitbox02-firmware/artifacts.sh
+++ bitbox02-firmware/artifacts.sh
@@ -3,7 +3,7 @@
 DATE=`date +%Y-%m-%d`
 TWITTER_NAME="BitBox02"
 URL="https://shiftcrypto.ch/bitbox02/"
-VERSION="firmware-btc-only/v9.12.0"
+VERSION="firmware-btc-only/v9.13.1"
 REPO="https://github.com/digitalbitbox/bitbox02-firmware"
 CHECKSUM_SOURCE="https://github.com/digitalbitbox/bitbox02-firmware/releases/tag/${VERSION}"
 PROJECT="bitbox02-firmware"
